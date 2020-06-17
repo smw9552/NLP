d39e55fd77bb08e5177416fb4d10a6714b2349b7
Myungwon Seo <seomyungwon@gmail.com>
2020-06-17 21:59:57 +0900

    Edit

--- Pubmed_API_Test.py
+++ Pubmed_API_Test.py
@@ -10,13 +10,15 @@ Keywords = []
 
 #최초 URL 접근해서 result page 숫자 추출
 
-Pubmed_Info_URL = "https://pubmed.ncbi.nlm.nih.gov/?term=ESR1+breast+cancer"
+#Pubmed_Info_URL = "https://pubmed.ncbi.nlm.nih.gov/?term=ESR1+breast+cancer"
+Pubmed_Info_URL = "https://pubmed.ncbi.nlm.nih.gov/?term=RAS%2C+Wnt%2C+e3+ligase"
 #Pubmed_Info_URL = "https://pubmed.ncbi.nlm.nih.gov/?term=ESR1%20breast%20cancer&page=50"
 
 Pubmed_Info = urlopen(Pubmed_Info_URL, None, timeout = 1000000)
 
 while True:
     Info_line = Pubmed_Info.readline()
+    #print(Info_line)
     if not Info_line: break
 
     #Result 숫자 추출
@@ -34,11 +36,12 @@ while True:
         page_result = int(math.ceil(page_result))
         print(page_result)
 
+
 PMID_List = []
 
-#각 page에서 PMID 추출
-for ai in range(0, page_result):
-    print(str("page") + str(ai))
+#각 page에서 PMID 추출, range 함수의 마지막 기준 숫자보다 -1 값으로 for문이 돌기 때문에 +1값 처리 진행
+for ai in range(1, page_result+1):
+    print(str("page") + str(ai) + str("\n"))
     Pubmed_Info_URL = "https://pubmed.ncbi.nlm.nih.gov/?term=ESR1%20breast%20cancer&page=" + str(ai)
 
     Pubmed_Info = urlopen(Pubmed_Info_URL, None, timeout = 1000000)
@@ -51,9 +54,20 @@ for ai in range(0, page_result):
         if str(Info_line).__contains__(str(">PMID: <span class=")):
             #print(Info_line)
 
-            print(str(Info_line).
-                  replace('''b'  <span class="citation-part">PMID: <span class="docsum-pmid">''','').
-                replace("</span></span>",'').
-                replace('\\n','').
-                replace("'",''))
-print("next page")
+            PMID = str(Info_line).replace('''b'  <span class="citation-part">PMID: <span class="docsum-pmid">''','').\
+                replace("</span></span>",'').\
+                replace('\\n','').\
+                replace("'",'')
+
+            print(PMID)
+
+            PMID_List.append(PMID)
+
+print(str("Extract PMID"))
+
+
+
+print(PMID_List)
+print(len(PMID_List))
+print(PMID_List[2])
+print(PMID_List[10])
---
+++ Pubtator_API_Test.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen
+
+Single_PMID = "28483577"
+PMIDs = []
+
+Pubtator_Info_URL = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator?pmids=28483577"
+
+Pubtator_Info_URL = urlopen()
