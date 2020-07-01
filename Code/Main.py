89bf7cdf3cb9c476836765f270716ed4fa145594
Myungwon Seo <seomyungwon@gmail.com>
2020-07-01 15:17:22 +0900

    Edit

--- Code/Main.py
+++ Code/Main.py
@@ -4,5 +4,10 @@ from Code.Pubmed import pubmed
 DATA = data()
 PUBMED = pubmed()
 
-DATA.input_two_keywords(str("garlic"), str("cancer"))
+# Space = %20, Comma = +
+Keywords = DATA.input_two_keywords(str("garlic%20extract"), str("anticancer"))
 
+PMID_List = PUBMED.extract_PMID_by_two_keywords(Keywords)
+
+print(PMID_List)
+print(len(PMID_List))
--- Code/Pubmed.py
+++ Code/Pubmed.py
@@ -5,22 +5,21 @@ class pubmed:
 
     def extract_PMID_by_two_keywords(self, Keywords):
 
-        # access pubmed website & search papers by using keywords
-        Pubmed_Info_URL = "http://pubmed.ncbi.nlm.nih.gov/?term=" + str(Keywords[0]) + str('+') + str(Keywords[1])
-        Pubmed_Info = urlopen(Pubmed_Info_URL, None, timeout=100000)
+        try:
+            # access pubmed website & search papers by using keywords
+            Pubmed_Info_URL = "http://pubmed.ncbi.nlm.nih.gov/?term=" + str(Keywords[0]) + str('+') + str(Keywords[1])
+            Pubmed_Info = urlopen(Pubmed_Info_URL, None, timeout=100000)
 
-        # extract all result page number
-        while True:
-            Info_line = Pubmed_Info.readline()
-            if not Info_line: break
+            # extract all result page number
+            while True:
+                Info_line = Pubmed_Info.readline()
+                if not Info_line: break
 
-            try:
                 if str(Info_line).__contains__(str('''<meta name="log_resultcount" content=''')):
-
-                    result = str(Info_line). \
-                        replace('''b'    <meta name="log_resultcount" content="''', ''). \
-                        replace('''" />''', ''). \
-                        replace('\\n', ''). \
+                    result = str(Info_line).\
+                        replace('''b'    <meta name="log_resultcount" content="''', '').\
+                        replace('''" />''', '').\
+                        replace('\\n', '').\
                         replace("'", '')
 
                     # 1 page 마다 10개의 PMID 결과가 제공 되기 때문에 10으로 나누어서 정리.
@@ -29,17 +28,11 @@ class pubmed:
                     page_result = int(math.ceil(page_result))
                     print(page_result)
 
-            except Exception:
-                print("error")
-
-            except TimeoutError:
-                print("Timeout Error")
-
-        try:
             # extract all PMID List (각각의 page에서 PMID 추출, range 함수의 마지막 기준 숫자보다 -1 값으로 for문이 돌아가기 때문에 +1 처리
             PMID_List = []
             for ai in range(1, page_result+1):
-                print(str("page") + str(ai) + str("\n"))
+
+                print(str("PMID search page= ") + str(ai))
                 Pubmed_PMID_URL = "http://pubmed.ncbi.nlm.nih.gov/?term=" + str(Keywords[0]) + str('+') + str(Keywords[1]) + str("&page=") + str(ai)
 
                 Pubmed_PMID = urlopen(Pubmed_PMID_URL, None, timeout=1000000)
@@ -48,24 +41,26 @@ class pubmed:
                     line = Pubmed_PMID.readline()
                     if not line: break
 
-                if str(line).__contains__(str(">PMID: <span class=")):
+                    if str(line).__contains__(str(">PMID: <span class=")):
 
-                    PMID = str(Info_line).replace('''b'  <span class="citation-part">PMID: <span class="docsum-pmid">''','').\
-                        replace("</span></span>", '').\
-                        replace('\\n', '').\
-                        replace("'", '')
+                        PMID = str(line).replace('''b'  <span class="citation-part">PMID: <span class="docsum-pmid">''','').\
+                            replace("</span></span>", '').\
+                            replace('\\n', '').\
+                            replace("'", '')
 
-                    PMID_List.append(PMID)
-                    Final_PMID_List = sorted(list(set(PMID_List)))
+                        print(PMID)
 
-                    return Final_PMID_List
-
-        except Exception:
-            print("error")
+                        PMID_List.append(PMID)
+                        Final_PMID_List = sorted(list(set(PMID_List)))
 
         except TimeoutError:
-            print("Timeout Error")
+            print("Time out error")
 
+        except Exception:
+            print ("Error")
+
+
+        return Final_PMID_List
 
 
 
